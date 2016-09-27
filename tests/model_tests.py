b751151d6c39521f3720bf948f5be98f81ab4531
John Edge <edge.jm@gmail.com>
2016-09-27 17:09:26 -0400

    removing virtualenv added testing and requirements

---
+++ tests/model_tests.py
@@ -0,0 +1,31 @@
+"""
+Testing module for recipeshelf models.  Using SQLAlchemy and Flask Testing
+modules.
+"""
+import sys
+from flask_testing import TestCase
+
+sys.path.insert(0, '../recipeshelf')
+from models import create_app, db
+
+class UserCreate(TestCase):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///var/www/recipeshelf/testing/test.db"
+    TESTING = True
+
+    def create_app(self):
+        # no idea what's supposed to go here
+        return create_app(self)
+
+    def setup(self):
+        db.create_all()
+
+    def teardown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_create(self):
+        user = User()
+        db.session.add(user)
+        db.session.commit()
+        assert user in db.session
+        response = self.client.get("/")
